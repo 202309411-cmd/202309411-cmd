c6fa7e81bb283a493a8b3a5d1e8fdbe8cfbed5ed
202309411-cmd <202309411@mondragonmexico.edu.mx>
2026-08-26 15:40:59 -0600

    Actualizacion de tarea

--- tarea_brazo.m
+++ tarea_brazo.m
@@ -5,18 +5,16 @@ teta1 = input("Introduce el angulo de la primer articulacion [rad]: ");
 l2 = input("Introduce la longitud del eslabon 2 [m]: ");
 teta2 = input("Introduce el angulo de la segunda articulacion [rad]: ");
 
-ruta1_teta1 = linspace(0, teta1, 30);
+ruta1_teta1 = linspace(0, teta1, 20);
 ruta1_teta2 = zeros(1, 30); 
 
-ruta2_teta1 = teta1 * ones(1, 30);
-ruta2_teta2 = linspace(0, teta2, 30);
+ruta2_teta1 = teta1 * ones(1, 20);
+ruta2_teta2 = linspace(0, teta2, 20);
 
 ruta_teta1 = [ruta1_teta1, ruta2_teta1];
 ruta_teta2 = [ruta1_teta2, ruta2_teta2];
 
 joint_1 = [0 0]';
-limite = l1 + l2 + 0.5;
-
 pasos_totales = length(ruta_teta1); 
 
 for i = 1:pasos_totales
@@ -24,11 +22,11 @@ for i = 1:pasos_totales
     hold on;
     grid on;
     
-    axis([-limite limite -limite limite]);
-    
-    line([0 2], [0 0], [0 0], 'Color', 'red', 'LineWidth', 3);
-    line([0 0], [0 2], [0 0], 'Color', 'green', 'LineWidth', 3);
-    
+    axis([0 3 0 3]); 
+
+    line([0 10], [0 0], 'Color', 'red', 'LineWidth', 3);
+    line([0 0], [0 10], 'Color', 'green', 'LineWidth', 3);
+
     angulo_actual = ruta_teta1(i);
     angulo_actual2 = ruta_teta2(i);
     
@@ -47,8 +45,8 @@ for i = 1:pasos_totales
     scatter(joint_2(1), joint_2(2), 100, 'filled', 'MarkerEdgeColor', 'blue');
     scatter(EF(1), EF(2), 100, 'filled', 'MarkerEdgeColor', 'blue');
     
-    line([joint_1(1) joint_2(1)], [joint_1(2) joint_2(2)], [0 0], "Color", "red", "LineWidth", 2, "LineStyle", "--");
-    line([joint_2(1) EF(1)], [joint_2(2) EF(2)], [0 0], "Color", "red", "LineWidth", 2, "LineStyle", "--");
+    line([joint_1(1) joint_2(1)], [joint_1(2) joint_2(2)], "Color", "red", "LineWidth", 2, "LineStyle", "--");
+    line([joint_2(1) EF(1)], [joint_2(2) EF(2)], "Color", "red", "LineWidth", 2, "LineStyle", "--");
     
-    pause(0.05); 
+    pause(0.00005); 
 end
